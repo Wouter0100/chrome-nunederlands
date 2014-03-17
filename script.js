27382721a8db68169f97b046cc998d5f3b7aea04
Wouter0100 <wouter@wouter0100.nl>
2014-03-17 11:07:58 +0100

    Improved script.

--- script.js
+++ script.js
@@ -2,19 +2,37 @@ var opdracht = ((typeof exerciseEABABA !== 'undefined') ? exerciseEABABA : null)
 
 if(opdracht != null) {
 
-    var textbox = $('textarea#EABABABA');
+	console.log(opdracht.type);
 
-    if(textbox.length != 0) {
-        textbox.val(opdracht.items[0].targetText.split('\n\n')[1]);
+	switch(opdracht.type) {
+			
+		case EX_TEXTENTRY:
+			var textbox = $('textarea#EABABABA');
+			
+			textbox.val(opdracht.items[0].targetText.split('\n\n')[1]);
+			opdracht.itemTextSelected(opdracht.getItem(textbox.attr("id")), textbox.val());
+			$('.okBtn').click();
+			
+			break;
+		
+		case EX_DRAGNSORT:
+		case EX_DRAGCONTAINER:
+		case EX_DRAGNDROPSTACKSENTENCE:	
+		case EX_DRAGNSTACK:
+		case EX_DRAGNDROP:
+		case EX_TEXTENTRYDROPLIST:
+		case EX_CONNECTION:
+		case EX_PAIRS:
+		case EX_SMARTASSET:
+		case EX_SINGLECHOICE:
+		case EX_MULTIPLECHOICE:
+		case EX_TRUEORFALSE:
+		case EX_SHAPECLICK:
+		default:
+			opdracht.showCorrectAnswer();
 
-        opdracht.itemTextSelected(opdracht.getItem(textbox.attr("id")), textbox.val());
-
-	$('.okBtn').click();
-    }else{
-        opdracht.showCorrectAnswer();
-
-	$('.selCorrect').removeClass('selCorrect').children('.sc').click();
-    }
-
-    //$('.okBtn').removeClass('inactive');
+			$('.selCorrect').removeClass('selCorrect').children('.sc').click();
+			break;
+			
+	}
 }
