5e626c1dcb9068bbfd6991ebe8e352ca0b845a0a
Wouter0100 <wouter@wouter0100.nl>
2014-03-17 11:09:51 +0100

    Some more improvements.

--- script.js
+++ script.js
@@ -7,20 +7,30 @@ if(opdracht != null) {
 	switch(opdracht.type) {
 			
 		case EX_TEXTENTRY:
-			var textbox = $('textarea#EABABABA');
+			var textbox = $('textarea#EABABABA:not(class="ext-mb-textarea")');
 			
-			textbox.val(opdracht.items[0].targetText.split('\n\n')[1]);
-			opdracht.itemTextSelected(opdracht.getItem(textbox.attr("id")), textbox.val());
-			$('.okBtn').click();
+			if(textbox != null){
+				textbox.val(opdracht.items[0].targetText.split('\n\n')[1]);
+				opdracht.itemTextSelected(opdracht.getItem(textbox.attr("id")), textbox.val());
+				$('.okBtn').click();
+			}else{
+				
+				opdracht.showCorrectAnswer();
+			}
 			
 			break;
-		
+			
+			
 		case EX_DRAGNSORT:
 		case EX_DRAGCONTAINER:
 		case EX_DRAGNDROPSTACKSENTENCE:	
-		case EX_DRAGNSTACK:
+		case EX_DRAGNSTACK:		
 		case EX_DRAGNDROP:
-		case EX_TEXTENTRYDROPLIST:
+			opdracht.showCorrectAnswer();
+			
+			$('.selCorrect').removeClass('selCorrect');
+			break;
+			
 		case EX_CONNECTION:
 		case EX_PAIRS:
 		case EX_SMARTASSET:
